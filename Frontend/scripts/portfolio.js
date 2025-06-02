968b4b3381c28742184c515de71a14a835dafa67
Tanishqsingh288 <singhtanishq288@gmail.com>
2025-06-02 20:32:53 +0530

    fix

---
+++ Frontend/scripts/portfolio.js
@@ -0,0 +1,13 @@
+// Scroll animation
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+document.querySelectorAll('.fade-in-up').forEach(section => {
+  observer.observe(section);
+});
+
